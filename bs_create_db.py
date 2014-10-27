5b4228245bdaba091c4098e9964652f3ac4cbc6e
lucasbrambrink <lbrambrink@gmail.com>
2014-10-27 02:40:40 -0400

    added foreign keys to DB

--- bs_create_db.py
+++ bs_create_db.py
@@ -20,21 +20,21 @@ class DB:
 
 
 		c.execute("""CREATE TABLE IF NOT EXISTS Player
-			(id INTEGER,
+			(id PRIMARY KEY,
 			 name TEXT,
 			 games_won INTEGER,
-			 latest_sign_in TEXT, 
-			 PRIMARY KEY ('id'))""")
+			 latest_sign_in TEXT)""")
 
 		c.execute("""CREATE TABLE IF NOT EXISTS Games
-			(id INTEGER, 
+			(id PRIMARY KEY, 
 			 player_id INTEGER,
 			 ai_id INTEGER, 
 			 time_created TEXT, 
-			 PRIMARY KEY ('id'))""")
+			 FOREIGN KEY ('ai_id') REFERENCES AI ('id'),
+			 FOREIGN KEY ('player_id') REFERENCES Player ('id'))""")
 
 		c.execute("""CREATE TABLE IF NOT EXISTS GameBoard
-			(id INTEGER, 
+			(id PRIMARY KEY, 
 			 player_id INTEGER, 
 			 game_id INTEGER, 
 			 size INTEGER, 
@@ -42,32 +42,32 @@ class DB:
 			 boat_list TEXT, 
 			 sunken_ships TEXT, 
 			 time_created TEXT, 
-			 PRIMARY KEY ('id'))""")
+			 FOREIGN KEY ('player_id') REFERENCES Player ('id')
+			 FOREIGN KEY ('game_id') REFERENCES Games ('id'))""")
 		
 		c.execute("""CREATE TABLE IF NOT EXISTS AI
-			(id INTEGER, 
+			(id PRIMARY KEY, 
 			 game_id INTEGER, 
 			 tries TEXT, 
 			 array_successes TEXT, 
-			 PRIMARY KEY ('id'))""")
+			 FOREIGN KEY ('game_id') REFERENCES Games ('id'))""")
 		
 		c.execute("""CREATE TABLE IF NOT EXISTS Boat
-			(id INTEGER, 
+			(id PRIMARY KEY, 
 			 board_id INTEGER, 
 			 name TEXT, 
 			 health INTEGER, 
 			 coordinates TEXT, 
-			 PRIMARY KEY ('id'))""")
+			 FOREIGN KEY ('board_id') REFERENCES GameBoard ('id'))""")
 
 		c.execute("""CREATE TABLE IF NOT EXISTS Success
-			(id INTEGER, 
+			(id PRIMARY KEY, 
 			 ai_id INTEGER, 
 			 cell TEXT, 
 			 periphery TEXT, 
 			 directions_explored TEXT, 
-			 PRIMARY KEY ('id'))""")
+			 FOREIGN KEY ('ai_id') REFERENCES AI ('id'))""")
 		
-	
 		conn.commit()
 		c.close()
 
