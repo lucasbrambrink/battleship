572d5e5ea94d9f5c40dc473b63d74de78343fc36
lucasbrambrink <lbrambrink@gmail.com>
2014-10-27 02:34:21 -0400

    cleaning up DB

--- bs_create_db.py
+++ bs_create_db.py
@@ -20,22 +20,52 @@ class DB:
 
 
 		c.execute("""CREATE TABLE IF NOT EXISTS Player
-			(id INTEGER, name TEXT, games_won INTEGER, latest_sign_in TEXT, PRIMARY KEY ('id'))""")
+			(id INTEGER,
+			 name TEXT,
+			 games_won INTEGER,
+			 latest_sign_in TEXT, 
+			 PRIMARY KEY ('id'))""")
 
 		c.execute("""CREATE TABLE IF NOT EXISTS Games
-			(id INTEGER, player_id INTEGER, ai_id INTEGER, time_created TEXT, PRIMARY KEY ('id'))""")
+			(id INTEGER, 
+			 player_id INTEGER,
+			 ai_id INTEGER, 
+			 time_created TEXT, 
+			 PRIMARY KEY ('id'))""")
 
 		c.execute("""CREATE TABLE IF NOT EXISTS GameBoard
-			(id INTEGER, player_id INTEGER, game_id INTEGER, size INTEGER, board TEXT, boat_list TEXT, sunken_ships TEXT time_created TEXT, PRIMARY KEY ('id'))""")
+			(id INTEGER, 
+			 player_id INTEGER, 
+			 game_id INTEGER, 
+			 size INTEGER, 
+			 board TEXT, 
+			 boat_list TEXT, 
+			 sunken_ships TEXT, 
+			 time_created TEXT, 
+			 PRIMARY KEY ('id'))""")
 		
 		c.execute("""CREATE TABLE IF NOT EXISTS AI
-			(id INTEGER, game_id INTEGER, tries TEXT, array_successes TEXT, PRIMARY KEY ('id'))""")
+			(id INTEGER, 
+			 game_id INTEGER, 
+			 tries TEXT, 
+			 array_successes TEXT, 
+			 PRIMARY KEY ('id'))""")
 		
 		c.execute("""CREATE TABLE IF NOT EXISTS Boat
-			(id INTEGER, board_id INTEGER, name TEXT, health INTEGER, coordinates TEXT, PRIMARY KEY ('id'))""")
+			(id INTEGER, 
+			 board_id INTEGER, 
+			 name TEXT, 
+			 health INTEGER, 
+			 coordinates TEXT, 
+			 PRIMARY KEY ('id'))""")
 
 		c.execute("""CREATE TABLE IF NOT EXISTS Success
-			(id INTEGER, ai_id INTEGER, cell TEXT, periphery TEXT, directions_explored TEXT, PRIMARY KEY ('id'))""")
+			(id INTEGER, 
+			 ai_id INTEGER, 
+			 cell TEXT, 
+			 periphery TEXT, 
+			 directions_explored TEXT, 
+			 PRIMARY KEY ('id'))""")
 		
 	
 		conn.commit()
